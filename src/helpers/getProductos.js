f306335d72589a5a42d101734bd2f5e1005fad26
JulianTM <jpachonl99@gmail.com>
2022-12-28 17:36:18 -0500

    edit peticion

--- src/helpers/getProductos.js
+++ src/helpers/getProductos.js
@@ -1,5 +1,5 @@
 export const getProductos = async() => {
-    const url = `https://api.escuelajs.co/api/v1/products?offset=10&limit=30`;
+    const url = `https://api.escuelajs.co/api/v1/products?offset=20&limit=40`;
     const resp = await fetch(url);
     const data = await resp.json();
 
@@ -8,7 +8,6 @@ export const getProductos = async() => {
             id: product.id,
             title: product.title,
             imagen: product.images[0],
-            // imagen: 'http://localhost:5173/img/092a93bd0e2f183f210e1840d2c126f4.jpg'
         }
     })
 
